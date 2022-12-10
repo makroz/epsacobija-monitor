81bf96c71fce1f03473bb3b413d58e98f7a16ebb
Mario Guzman PC <makroz@hotmail.com>
2022-12-10 15:30:17 -0400

    personalizacion y cambio de logos

--- components/AppDrawer.vue
+++ components/AppDrawer.vue
@@ -11,27 +11,21 @@
     <v-toolbar color="primary darken-1" dark>
       <!-- colocar estos titulos en store o config -->
       <v-toolbar-side-icon @click.stop="toggleDrawer()"></v-toolbar-side-icon>
-      <img src="../static/logo.png" height="36" alt="Cosmol" />
-      <v-toolbar-title class="ml-0 pl-3">Cosmol</v-toolbar-title>
+      <img src="../static/logo.png" height="36" alt="Sistema EPSA Cobija" />
+      <v-toolbar-title class="ml-0 pl-3">EPSA Cobija</v-toolbar-title>
       <v-spacer></v-spacer>
-      <img
-        src="../static/m.png"
-        height="36"
-        alt="Sistema Unicef/Cosmol"
-        class="ml-1"
-      />
+      <img src="../static/m.png" height="36" alt="Sistema EPSA Cobija" class="ml-1" />
     </v-toolbar>
-    <vue-perfect-scrollbar
-      class="drawer-menu--scroll"
-      :settings="scrollSettings"
-    >
+    <vue-perfect-scrollbar class="drawer-menu--scroll" :settings="scrollSettings">
       <v-list dense>
-        <v-subheader> Principal v.{{ version }}</v-subheader>
+        <v-subheader>Principal v.{{ version }}</v-subheader>
         <!-- level 1 begin-->
         <template v-for="(item, i) in menus">
-          <v-subheader v-if="item.header" :key="i">{{
+          <v-subheader v-if="item.header" :key="i">
+            {{
             item.header
-          }}</v-subheader>
+            }}
+          </v-subheader>
           <v-divider v-else-if="item.divider" :key="i"></v-divider>
 
           <v-list-tile
@@ -63,7 +57,7 @@
                 :disabled="item2.disabled"
                 :target="item2.target"
               >
-                <v-list-tile-action> </v-list-tile-action>
+                <v-list-tile-action></v-list-tile-action>
                 <v-list-tile-title>{{ item2.title }}</v-list-tile-title>
                 <v-list-tile-action>
                   <v-icon>{{ item2.icon }}</v-icon>
@@ -85,10 +79,7 @@
                   :target="item3.target"
                   class="blue lighten-5"
                 >
-                  <v-list-tile-title
-                    class="pl-2"
-                    v-text="item3.title"
-                  ></v-list-tile-title>
+                  <v-list-tile-title class="pl-2" v-text="item3.title"></v-list-tile-title>
                   <v-list-tile-action>
                     <v-icon v-text="item3.icon"></v-icon>
                   </v-list-tile-action>
@@ -101,14 +92,10 @@
         </template>
         <!-- level 1 end -->
       </v-list>
-      <v-btn small class="error" @click="reload()">
-        Recargar Pagina
-      </v-btn>
-      <br>
-      <br>
-       <v-btn small class="success" @click="clearOffline()">
-        Limpiar OFFLINE
-      </v-btn>
+      <v-btn small class="error" @click="reload()">Recargar Pagina</v-btn>
+      <br />
+      <br />
+      <v-btn small class="success" @click="clearOffline()">Limpiar OFFLINE</v-btn>
     </vue-perfect-scrollbar>
   </v-navigation-drawer>
 </template>
@@ -158,10 +145,10 @@ export default {
     toggleDrawer() {
       this.$store.commit('toggleDrawer')
     },
-     clearOffline(mod = false) {
-          localStorage.removeItem('colaSave')
-          alert('Transacciones OFFLINE Eliminadas....')
-    },
+    clearOffline(mod = false) {
+      localStorage.removeItem('colaSave')
+      alert('Transacciones OFFLINE Eliminadas....')
+    }
   }
 }
 </script>
